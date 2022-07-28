aad73f7305ae50244bd900f62119a5815fe07778
Konstantin Vlahakis <kostakv@Outlook.com>
2022-07-28 18:34:40 -0400

    formated DayList.js to clean up code

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,20 +1,20 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList(props){
+export default function DayList(props) {
   const days = props.days;
   const listDays = days.map((day) => {
-    return <DayListItem 
+    return <DayListItem
       key={day.id}
       name={day.name}
       spots={day.spots}
       setDay={props.onChange}
       selected={day.name === props.value}
-      />
-  })
+    />;
+  });
   return (
     <ul>
       {listDays}
     </ul>
-  )
+  );
 }
